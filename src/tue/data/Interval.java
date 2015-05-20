b6c9d43ff1132ec89c4c9aca5d496f502cf60a93
LuukvH <laj@home.nl>
2015-05-20 11:00:37 +0200

    Implemented cross and plus

--- src/tue/data/Interval.java
+++ src/tue/data/Interval.java
@@ -1,17 +1,11 @@
 package tue.data;
 
+import java.util.BitSet;
+
 public class Interval {
     private Integer time1;
     private Integer time2;
     private boolean isEmpty = true;
-
-    public static Interval empty() {
-        Interval interval = new Interval();
-        interval.isEmpty = true;
-        return interval;
-    }
-
-    private Interval() {}
 
     public Interval(Integer time1, Integer time2) {
         this.time1 = time1;
@@ -19,17 +13,13 @@ public class Interval {
         this.isEmpty = false;
     }
 
-    public boolean contains(Integer given) {
-        return time1.compareTo(given) >= 0 && given.compareTo(time2) <= 0;
-    }
-
-    public Interval intersect(Interval i2) {
-        return i2;
-    }
-
-    public boolean isEmpty() {
-        return isEmpty;
-    }
+//    public boolean contains(Integer given) {
+//        return time1.compareTo(given) >= 0 && given.compareTo(time2) <= 0;
+//    }
+//
+//    public Interval intersect(Interval i2) {
+//        return i2;
+//    }
 
     public Integer getStartTime() {
         return time1;
